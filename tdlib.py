01f2bc18c29865dfd1615a02866a23a9821107b6
Sirenfal <avosirenfal@gmail.com>
2024-05-16 04:32:30 -0400

    * Initial commit, tdlib-json test

---
+++ tdlib.py
@@ -0,0 +1,81 @@
+from ctypes.util import find_library
+from ctypes import *
+import json
+import os
+import sys
+import platform
+
+_log_message_callback_type = CFUNCTYPE(None, c_int, c_char_p)
+
+class TDLib:
+    _instance = None
+    _tdjson_path = None
+    _tdjson = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(TDLib, cls).__new__(cls)
+            cls._instance._load_tdjson()
+            cls._instance._initialize_tdjson()
+            cls._instance._set_log_message_callback()
+        return cls._instance
+
+    def _load_tdjson(self):
+        if self._tdjson_path is None:
+            self._tdjson_path = find_library('tdjson')
+            if self._tdjson_path is None:
+                if os.name == 'nt':
+                    self._tdjson_path = os.path.join(os.path.dirname(__file__), "natives", platform.system().lower(), "tdjson.dll")
+                else:
+                    sys.exit("Can't find 'tdjson' library")
+            self._tdjson = CDLL(self._tdjson_path)
+
+    def _initialize_tdjson(self):
+        self._td_create_client_id = self._tdjson.td_create_client_id
+        self._td_create_client_id.restype = c_int
+        self._td_create_client_id.argtypes = []
+
+        self._td_receive = self._tdjson.td_receive
+        self._td_receive.restype = c_char_p
+        self._td_receive.argtypes = [c_double]
+
+        self._td_send = self._tdjson.td_send
+        self._td_send.restype = None
+        self._td_send.argtypes = [c_int, c_char_p]
+
+        self._td_execute = self._tdjson.td_execute
+        self._td_execute.restype = c_char_p
+        self._td_execute.argtypes = [c_char_p]
+
+        self._td_set_log_message_callback = self._tdjson.td_set_log_message_callback
+        self._td_set_log_message_callback.restype = None
+        self._td_set_log_message_callback.argtypes = [c_int, _log_message_callback_type]
+
+    @_log_message_callback_type
+    @staticmethod
+    def _on_log_message_callback(verbosity_level, message):
+        if verbosity_level == 0:
+            sys.exit('TDLib fatal error: %r' % message)
+
+    def _set_log_message_callback(self):
+        self._td_set_log_message_callback(2, self._on_log_message_callback)
+
+    def td_execute(self, query):
+        query = json.dumps(query).encode('utf-8')
+        result = self._td_execute(query)
+        if result:
+            result = json.loads(result.decode('utf-8'))
+        return result
+
+    def create_client_id(self):
+        return self._td_create_client_id()
+
+    def send(self, client_id, query):
+        query = json.dumps(query).encode('utf-8')
+        self._td_send(client_id, query)
+
+    def receive(self):
+        result = self._td_receive(1.0)
+        if result:
+            result = json.loads(result.decode('utf-8'))
+        return result
