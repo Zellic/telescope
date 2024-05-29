9e0020512812d5902e07c9154469329ef7c10883
Sirenfal <avosirenfal@gmail.com>
2024-05-29 17:44:04 -0400

    * To support multiple clients at once manager needs to do event dispatch instead of trying to use threadpool async

--- telegram/client.py
+++ telegram/client.py
@@ -6,23 +6,19 @@ from telegram.tdlib import TDLib
 
 class TelegramClient:
 	def __init__(self, auth: AuthenticationScheme, modules: List[any]):
-		self.tdlib = TDLib()
+		# noinspection PyTypeChecker
+		self._tdlib: TDLib = None
 		self.auth = auth
-		self.client_id = self.tdlib.create_client_id()
+		self.client_id = None
 
 		self._modules = modules or []
 
 		self._extra_counter = 0
 		self._pending_responses = {}
-		self._lock = asyncio.Lock()
-		self._stop_event = asyncio.Event()
-		self._task = None
-		# errors
-		self.tdlib.td_execute({'@type': 'setLogVerbosityLevel', 'new_verbosity_level': 1})
 		self._started = False
 
 	def send(self, query):
-		self.tdlib.send(self.client_id, query)
+		self._tdlib.send(self.client_id, query)
 
 	"""
 	send a tdlib query, awaiting a reply
@@ -30,61 +26,53 @@ class TelegramClient:
 	if used with a query type that does not cause a response this will await forever
 	"""
 	async def sendAwaitingReply(self, query):
-		async with self._lock:
-			self._extra_counter += 1
-			extra = self._extra_counter
+		self._extra_counter += 1
+		extra = self._extra_counter
 
 		query['@extra'] = extra
 		future = asyncio.get_event_loop().create_future()
 
-		async with self._lock:
-			self._pending_responses[extra] = future
+		self._pending_responses[extra] = future
 
 		self.send(query)
 
 		return await future
 
-	async def _receive_loop(self):
-		while not self._stop_event.is_set():
-			event = await asyncio.to_thread(self.tdlib.receive)
+	def _event_received(self, event):
+		extra = event.get('@extra')
+		if extra is not None:
+			future = self._pending_responses.pop(extra, None)
 
-			if event is None:
-				continue
+			if future is not None:
+				future.set_result(event)
+				# a future handled this
+				return
 
-			extra = event.get('@extra')
-			if extra is not None:
-				async with self._lock:
-					future = self._pending_responses.pop(extra, None)
+		if event['@type'] == 'updateAuthorizationState':
+			auth_state = event['authorization_state']
 
-				if future is not None:
-					future.set_result(event)
-					# loop because a future handled this
-					continue
+			if auth_state['@type'] == 'authorizationStateClosed':
+				# TODO: do something about this
+				print("Authorization failed.")
+				return
+			elif auth_state['@type'] == 'authorizationStateReady':
+				print("Authorization successful.")
+			else:
+				method = getattr(self.auth, auth_state['@type'], None)
 
-			if event['@type'] == 'updateAuthorizationState':
-				auth_state = event['authorization_state']
+				if method is None:
+					print(f"Unimplemented authorization state: {auth_state['@type']}")
+					raise NotImplementedError()
 
-				if auth_state['@type'] == 'authorizationStateClosed':
-					print("Authorization failed.")
-					break
-				elif auth_state['@type'] == 'authorizationStateReady':
-					print("Authorization successful.")
-				else:
-					method = getattr(self.auth, auth_state['@type'], None)
+				method(self)
+				return
 
-					if method is None:
-						print(f"Unimplemented authorization state: {auth_state['@type']}")
-						raise NotImplementedError()
+		for module in self._modules:
+			func = getattr(module, event['@type'], None)
 
-					method(self)
-					continue
-
-			for module in self._modules:
-				func = getattr(module, event['@type'], None)
-
-				if(func is not None):
-					# noinspection PyAsyncCall
-					asyncio.create_task(func(self, event))
+			if(func is not None):
+				# noinspection PyAsyncCall
+				asyncio.create_task(func(self, event))
 
 	def is_started(self):
 		return self._started
@@ -93,25 +81,39 @@ class TelegramClient:
 		if(self.is_started()):
 			raise Exception("Already started")
 
-		self._started = True
-		self._task = asyncio.get_event_loop().create_task(self._receive_loop())
+		self.client_id = self._tdlib.create_client_id()
 		self.send({'@type': 'getOption', 'name': 'version', '@extra': 1.01234})
 
-	async def stop(self):
-		if(not self.is_started()):
+	def stop(self):
+		if (not self.is_started()):
 			raise Exception("Hasn't been started yet")
 
 		for module in self._modules:
 			func = getattr(module, "onClientExit", None)
 
-			if(func is not None):
+			if (func is not None):
 				func()
 
-		self._stop_event.set()
-		await self._task
-
-	# async def on_message(self, event):
-	# 	if event['@type'] == 'updateConnectionState':
-	# 		if event['state']['@type'] == 'connectionStateReady':
-	# 			# TODO: modules
-	# 			wot = await self.sendAwaitingReply({'@type': 'getChats', 'limit': 100})
+	# def is_started(self):
+	# 	return self._started
+	#
+	# def start(self):
+	# 	if(self.is_started()):
+	# 		raise Exception("Already started")
+	#
+	# 	self._started = True
+	# 	self._task = asyncio.get_event_loop().create_task(self._receive_loop())
+	# 	self.send({'@type': 'getOption', 'name': 'version', '@extra': 1.01234})
+	#
+	# async def stop(self):
+	# 	if(not self.is_started()):
+	# 		raise Exception("Hasn't been started yet")
+	#
+	# 	for module in self._modules:
+	# 		func = getattr(module, "onClientExit", None)
+	#
+	# 		if(func is not None):
+	# 			func()
+	#
+	# 	self._stop_event.set()
+	# 	await self._task
--- telegram/manager.py
+++ telegram/manager.py
@@ -1,15 +1,76 @@
+import asyncio
 from typing import Dict, List
 
 from telegram.client import TelegramClient
+from telegram.tdlib import TDLib
 
 
 class TelegramClientManager:
     def __init__(self):
         self.clients: List[TelegramClient] = []
+        self.tdlib = TDLib()
+        # set log level to error
+        self.tdlib.td_execute({'@type': 'setLogVerbosityLevel', 'new_verbosity_level': 1})
+
+        self._extra_counter = 0
+        self._pending_responses = {}
+        self._lock = asyncio.Lock()
+        self._stop_event = asyncio.Event()
+        self._task = None
+        self._started = False
 
     def add_client(self, client: TelegramClient):
         self.clients.append(client)
-        client.start()
+        # gross hack, difficult to conveniently fix without splitting this into two classes
+        # let's just not!
+        client._tdlib = self.tdlib
+
+    def _try_dispatch_event(self):
+        event = self.tdlib.receive()
+
+        if(event is None):
+            return False
+
+        client_id = event['@client_id']
+        client = next((x for x in self.clients if x.client_id == client_id), None)
+
+        if(client is None):
+            print(f"[!] Unhandled message for client ID: {client_id}")
+            return True
+
+        # noinspection PyProtectedMember
+        client._event_received(event)
+        return True
+
+    async def _receive_loop(self):
+        while not self._stop_event.is_set():
+            if(not self._try_dispatch_event()):
+                # we didn't get an event, let's give our other coroutines time to do some stuff
+                await asyncio.sleep(1)
+
+    def is_started(self):
+        return self._started
+
+    def start(self):
+        if (self.is_started()):
+            raise Exception("Already started")
+
+        for client in self.clients:
+            client.start()
+
+        self._started = True
+        self._task = asyncio.get_event_loop().create_task(self._receive_loop())
+        return self._task
+
+    async def stop(self):
+        if (not self.is_started()):
+            raise Exception("Hasn't been started yet")
+
+        for client in self.clients:
+            client.stop()
+
+        self._stop_event.set()
+        await self._task
 
     # TODO: some way to call .stop() on the client have it bubble up and remove it from this list
     # or alternatively you call stop on the manager with some client id?
