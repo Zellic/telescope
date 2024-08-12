7915f9f8f3ec986647fbe898230c92690d213ebf
Sirenfal <avosirenfal@gmail.com>
2024-08-11 23:25:22 -0400

    * Two factor!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from mainloop import MainLoop
 from telegram.auth.api import APIAuth
+from telegram.auth.base import StaticSecrets
 from telegram.auth.schemes.live import TelegramProduction
 from telegram.client import TelegramClient
 from telegram.tgmodules.getcode import GetAuthCode
@@ -11,8 +12,8 @@ from telegram.tgmodules.userinfo import UserInfo
 def main():
 	core = MainLoop()
 
-	def clientForClosure(phonenumber, username: Optional[str]=None):
-		scheme = TelegramProduction(core.API_ID, core.API_HASH, "accounts/" + phonenumber, True)
+	def clientForClosure(phonenumber, username: Optional[str]=None, secrets: Optional[StaticSecrets]=None):
+		scheme = TelegramProduction(core.API_ID, core.API_HASH, "accounts/" + phonenumber, secrets, True)
 		return TelegramClient(APIAuth(phonenumber, scheme), [
 			UserInfo(
 				phonenumber,
@@ -24,7 +25,11 @@ def main():
 		])
 
 	for account in core.accounts.getAccounts():
-		core.addClient(clientForClosure(account.phone_number, account.username))
+		core.addClient(clientForClosure(
+			account.phone_number,
+			account.username,
+			None if account.two_factor_password is None else StaticSecrets(account.two_factor_password)
+		))
 
 	core.mainLoop(clientForClosure)
 
