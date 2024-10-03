ee7dcef612ccc70ead1a8401b7cef078c3fff801
rose <47357290+rose4096@users.noreply.github.com>
2024-10-03 13:38:21 -0400

    Implement socket updates when needed and more commands

--- telegram/webapp/websocket/websocket.py
+++ telegram/webapp/websocket/websocket.py
@@ -1,26 +1,35 @@
+import asyncio
 import json
+import sys
 from enum import Enum
 from json import JSONDecodeError
 
 from quart import websocket, Blueprint, request
 
 from database.accesscontrol import Privilege
+from telegram.auth.api import ClientNotStarted
 from telegram.auth.schemes.staging import TelegramStaging
+from telegram.tgmodules.userinfo import UserInfo
 from telegram.util import Environment
 from telegram.webapp.websocket.util import tg_client_blob, get_webapp
 
 websocket_bp = Blueprint('socket_bp', __name__)
 
-
 class MessageSendType(str, Enum):
     CLIENT_START = 'CLIENT_START'
     ADD_TEST_ACCOUNT_RESPONSE = "ADD_TEST_ACCOUNT_RESPONSE"
     SUBMIT_VALUE_RESPONSE = "SUBMIT_VALUE_RESPONSE"
+    DELETE_ACCOUNT_RESPONSE = "DELETE_ACCOUNT_RESPONSE"
+    CONNECT_CLIENT_RESPONSE = "CONNECT_CLIENT_RESPONSE"
+    DISCONNECT_CLIENT_RESPONSE = "DISCONNECT_CLIENT_RESPONSE"
 
 
 class MessageRecvType(str, Enum):
     ADD_TEST_ACCOUNT = 'ADD_TEST_ACCOUNT'
     SUBMIT_VALUE = 'SUBMIT_VALUE'
+    DELETE_ACCOUNT = "DELETE_ACCOUNT"
+    CONNECT_CLIENT = "CONNECT_CLIENT"
+    DISCONNECT_CLIENT = "DISCONNECT_CLIENT"
 
 
 class Websocket:
@@ -71,7 +80,6 @@ class Websocket:
         if result.success:
             await self.webapp.client_manager.add_client(self.webapp.clientFor(phone))
             print(f"added account successfully ({phone})")
-            await self.send_clients()
             await self.send_ok_response(MessageSendType.ADD_TEST_ACCOUNT_RESPONSE)
         else:
             print(f"failed to add account: {result.error_message}")
@@ -109,7 +117,6 @@ class Websocket:
 
             try:
                 matching_client.auth.status.provideValue(value)
-                await self.send_clients()
                 await self.send_ok_response(MessageSendType.SUBMIT_VALUE_RESPONSE)
             except Exception as e:
                 await self.send_error_response(MessageSendType.SUBMIT_VALUE_RESPONSE,
@@ -117,9 +124,130 @@ class Websocket:
         except Exception as e:
             await self.send_error_response(MessageSendType.SUBMIT_VALUE_RESPONSE, f"Unexpected error: {str(e)}")
 
+    async def delete_account(self, data):
+        if not 'phone' in data:
+            await self.send_error_response(MessageSendType.DELETE_ACCOUNT_RESPONSE, "Invalid payload structure")
+            return
+
+        phone = data['phone']
+
+        client = next((x for x in self.webapp.client_manager.clients if x.auth.phone == phone), None)
+
+        if client is None:
+            await self.send_error_response(MessageSendType.DELETE_ACCOUNT_RESPONSE,
+                                           f"Couldn't find client with phone number: {phone}")
+            return
+
+        privileges = await self.webapp.privilegesFor(request, await self.webapp.get_tg_account(phone))
+
+        if Privilege.REMOVE_ACCOUNT not in privileges:
+            await self.send_error_response(MessageSendType.DELETE_ACCOUNT_RESPONSE,
+                                           f"Insufficient privileges to delete account {phone}")
+            return
+
+        async def shutdown_and_remove():
+            if client is not None and not client.is_stopped():
+                if not client.is_stopping():
+                    await client.stop()
+                else:
+                    # noinspection PyProtectedMember
+                    await client._stop_future
+
+            self.webapp.client_manager.clients.remove(client)
+            result = await self.webapp.account_manager.delete_account(phone)
+
+            if not result.success:
+                sys.stderr.write(
+                    f"[!] Could not delete account {phone}: {result.error_code} / {result.error_message}\n")
+
+        await asyncio.create_task(shutdown_and_remove())
+        await self.send_ok_response(MessageSendType.DELETE_ACCOUNT_RESPONSE)
+
+    async def connect_client(self, data):
+        if not 'phone' in data:
+            await self.send_error_response(MessageSendType.CONNECT_CLIENT_RESPONSE, "Invalid payload structure")
+            return
+
+        phone = data['phone']
+
+        client = next((x for x in self.webapp.client_manager.clients if x.auth.phone == phone), None)
+
+        if client is None:
+            await self.send_error_response(MessageSendType.CONNECT_CLIENT_RESPONSE,
+                                           f"Couldn't find client with phone number: {phone}")
+            return
+
+        privileges = await self.webapp.privilegesFor(request, await self.webapp.get_tg_account(phone))
+
+        if Privilege.MANAGE_CONNECTION_STATE not in privileges:
+            await self.send_error_response(MessageSendType.CONNECT_CLIENT_RESPONSE,
+                                           f"Insufficient privileges to manage connection state for {phone}")
+            return
+
+        if client.is_started():
+            await self.send_error_response(MessageSendType.CONNECT_CLIENT_RESPONSE, f"Client is already started")
+            return
+
+        await client.start()
+        await self.send_ok_response(MessageSendType.CONNECT_CLIENT_RESPONSE)
+
+    async def disconnect_client(self, data):
+        if not 'phone' in data:
+            await self.send_error_response(MessageSendType.DISCONNECT_CLIENT_RESPONSE, "Invalid payload structure")
+            return
+
+        phone = data['phone']
+
+        client = next((x for x in self.webapp.client_manager.clients if x.auth.phone == phone), None)
+
+        if client is None:
+            await self.send_error_response(MessageSendType.DISCONNECT_CLIENT_RESPONSE,
+                                           f"Couldn't find client with phone number: {phone}")
+            return
+
+        privileges = await self.webapp.privilegesFor(request, await self.webapp.get_tg_account(phone))
+
+        if Privilege.MANAGE_CONNECTION_STATE not in privileges:
+            await self.send_error_response(MessageSendType.DISCONNECT_CLIENT_RESPONSE,
+                                           f"Insufficient privileges to manage connection state for {phone}")
+            return
+
+        if not client.is_started():
+            await self.send_error_response(MessageSendType.DISCONNECT_CLIENT_RESPONSE, f"Client was not started")
+            return
+
+        if client.is_stopped():
+            await self.send_error_response(MessageSendType.DISCONNECT_CLIENT_RESPONSE, f"Client is already stopped")
+            return
+
+        if client.is_stopping():
+            await self.send_error_response(MessageSendType.DISCONNECT_CLIENT_RESPONSE, f"Client is already stopping")
+            return
+
+        async def shutdown_client():
+            info_module = next((x for x in client._modules if isinstance(x, UserInfo)), None)
+            info = None if info_module is None else info_module.info
+
+            await client.stop()
+
+            self.webapp.client_manager.clients.remove(client)
+            client.auth.status = ClientNotStarted()
+            await self.webapp.client_manager.add_client(client,
+                                                        start=False)  # clientFor(phone, None if info is None else info.db_username(), _get_account(phone)), False)
+
+        await asyncio.create_task(shutdown_client())
+        await self.send_ok_response(MessageSendType.DISCONNECT_CLIENT_RESPONSE)
+
     async def run(self):
-        message = await websocket.receive()
-        print(message)
+        if self.webapp.client_manager.socket_needs_update():
+            await self.send_clients()
+            self.webapp.client_manager.update_socket()
+
+        try:
+            message = await asyncio.wait_for(websocket.receive(), timeout=1)
+        except asyncio.TimeoutError:
+            message = None
+
         if message is None:
             return
 
@@ -129,11 +257,22 @@ class Websocket:
             print('Received invalid JSON message')
             return
 
-        match msg['type']:
+        msg_type: MessageRecvType = msg['type']
+        match msg_type:
             case MessageRecvType.SUBMIT_VALUE:
-                await self.submit_value(msg['data'])
+                data = msg['data']
+                await self.submit_value(data)
             case MessageRecvType.ADD_TEST_ACCOUNT:
                 await self.add_test_account()
+            case MessageRecvType.DELETE_ACCOUNT:
+                data = msg['data']
+                await self.delete_account(data)
+            case MessageRecvType.CONNECT_CLIENT:
+                data = msg['data']
+                await self.connect_client(data)
+            case MessageRecvType.DISCONNECT_CLIENT:
+                data = msg['data']
+                await self.disconnect_client(data)
 
 
 @websocket_bp.websocket('/socket')
